87b41523fa55cef512ccda5a64fc928bb353bde1
HordieikoVS <HordieikoVS@Hordieiko>
2015-12-01 15:34:48 +0200

    Add PORT

--- sitemap-tartustour-russia/src/main/java/net/mpopov/sitemap/SitemapGenerator.java
+++ sitemap-tartustour-russia/src/main/java/net/mpopov/sitemap/SitemapGenerator.java
@@ -1,16 +1,10 @@
 package net.mpopov.sitemap;
 
-import org.apache.log4j.Logger;
-import org.jsoup.HttpStatusException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import com.redfin.sitemapgenerator.WebSitemapGenerator;
+import generated.Configuration;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
@@ -21,7 +15,14 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import generated.Configuration;
+import org.apache.log4j.Logger;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.redfin.sitemapgenerator.WebSitemapGenerator;
 
 public class SitemapGenerator
 {
@@ -31,17 +32,29 @@ public class SitemapGenerator
     private static final String DOMAIN = Configuration.getInstance()
             .getDomain();
 
+    private static final String PORT = Configuration.getInstance().getPort();
+
     private static final String A_HREF_CSS_QUERY = "a[href]";
 
     private static Set<String> allUrl = new HashSet<String>();
+    
+    private static Set<String> Urls = new HashSet<String>();
 
     private static Queue<String> pageUrl = new LinkedList<String>();
 
     public static void main(String[] args) throws IOException,
             InterruptedException
     {
+        System.setProperty("java.net.useSystemProxies", "true");
         breadthFirstSearch();
+        deletePortFromUrl();
         createSiteMap();
+    }
+
+    private static void deletePortFromUrl()
+    {
+        for (String url : allUrl)
+            Urls.add(url.replace(DOMAIN + PORT, DOMAIN));
     }
 
     private static void breadthFirstSearch() throws IOException,
@@ -58,7 +71,7 @@ public class SitemapGenerator
 
         int batchSize = Configuration.getInstance().getBatchSize();
 
-        pageUrl.offer(DOMAIN);
+        pageUrl.offer(DOMAIN + PORT);
 
         int page = 0;
 
@@ -67,6 +80,8 @@ public class SitemapGenerator
             String link = pageUrl.poll();
             if (!allUrl.contains(link))
             {
+                LOGGER.info("link: " + link);
+
                 if (++page % batchSize == 0)
                     TimeUnit.SECONDS.sleep(delayBetweenPages);
 
@@ -94,6 +109,12 @@ public class SitemapGenerator
                     LOGGER.error(message);
                     continue;
                 }
+                catch (ConnectException e)
+                {
+                    String message = "ConnectException for link: " + link;
+                    LOGGER.error(message);
+                    continue;
+                }
 
                 allUrl.add(link);
 
@@ -113,10 +134,13 @@ public class SitemapGenerator
                             }
 
                         if (allowable)
-                            if (linkHref.contains(DOMAIN))
+                            if (linkHref.startsWith(DOMAIN + PORT))
                                 pageUrl.offer(linkHref);
+                            else if (linkHref.startsWith(DOMAIN))
+                                pageUrl.offer(linkHref.replace(DOMAIN, DOMAIN
+                                        + PORT));
                             else
-                                pageUrl.offer(DOMAIN + linkHref);
+                                pageUrl.offer(DOMAIN + PORT + linkHref);
                     }
                 }
             }
@@ -125,13 +149,13 @@ public class SitemapGenerator
 
     private static void createSiteMap() throws MalformedURLException
     {
-        if (!allUrl.isEmpty())
+        if (!Urls.isEmpty())
         {
             File baseDir = new File(Configuration.getInstance()
                     .getTempDirectory());
             WebSitemapGenerator wsg = new WebSitemapGenerator(DOMAIN, baseDir);
 
-            for (String url : allUrl)
+            for (String url : Urls)
                 wsg.addUrl(url);
 
             wsg.write();
